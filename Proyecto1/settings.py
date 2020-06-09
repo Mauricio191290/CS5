b3f7f9dedfbff641740b6322dc45b13ecb1159b2
mauricio morales martinez <191290@ids.upchiapas.edu.mx>
2020-06-08 20:34:59 -0500

    error heroku

--- Proyecto1/settings.py
+++ Proyecto1/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#^kevtebdq01z+t0u_%@5x370enn_n9cw6%it2&m1_y3ojv!1@'
+#SECRET_KEY = '#^kevtebdq01z+t0u_%@5x370enn_n9cw6%it2&m1_y3ojv!1@'
+SECRET_KEY = 'Secret'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
