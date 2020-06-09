bb7810798d551ebed9f03b25ac6d1a16062bf781
mauricio morales martinez <191290@ids.upchiapas.edu.mx>
2020-06-08 20:52:42 -0500

    error heroko2

--- Proyecto1/settings.py
+++ Proyecto1/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'Secret'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
 
-ALLOWED_HOSTS = ["csheroku5.herokuapp.com"]
+ALLOWED_HOSTS = ['csheroku5.herokuapp.com']
 
 
 # Application definition
