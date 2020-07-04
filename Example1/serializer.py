f3ad4ceb68e05d5df00279832805de620c44787f
mauricio morales martinez <191290@ids.upchiapas.edu.mx>
2020-07-04 00:15:06 -0500

    local changes 2

--- Example1/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers, serializers, viewsets
-
-from Example1.models import Example1
-
-class ExampleSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Example1
-        fields = ('__all__')
