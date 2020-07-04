ff1ccfd6383dceeb4bed9d88d240d8e4a9db997d
mauricio morales martinez <191290@ids.upchiapas.edu.mx>
2020-07-03 21:29:35 -0500

    put done

--- Example1/models.py
+++ Example1/models.py
@@ -2,16 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+# Create your models here.
 class Example1(models.Model):
-    name= models.CharField(max_length = 255,null = False, default = 'null')
-    edad = models.CharField(max_length=2,null = False, default = 'null')
-    direccion = models.CharField(max_length = 200,null = False, default = 'null')
-    curp = models.CharField(max_length = 16, null = False, default = 'null')
+    
+    name = models.CharField(max_length= 255, null = False, default = "Null")
+    edad = models.CharField(max_length= 255, null = False, default = "Null")
+    direccion = models.CharField(max_length= 255, null = False, default = "Null")
+    curp = models.CharField(max_length= 255, null = False, default = "Null")
+
 
     def __str__(self):
         return self.name
-
+        
     class Meta:
         db_table = 'Example1'
-
-    
