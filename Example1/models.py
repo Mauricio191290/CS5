6e86151d3267c75cdb8fb63a4be48da4a2683b82
mauricio morales martinez <191290@ids.upchiapas.edu.mx>
2020-07-03 23:08:32 -0500

    try5

--- Example1/models.py
+++ Example1/models.py
@@ -2,16 +2,14 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
 
-# Create your models here.
+
 class Example1(models.Model):
     
     name = models.CharField(max_length= 255, null = False)
-    edad = models.IntegerField(validators=[MaxValueValidator(99), MinValueValidator(1)])
-    direccion = models.CharField(max_length= 200)
-    curp = models.CharField(max_length= 16,validators = [MinLengthValidator(16)])
-
+    edad =models.IntegerField(validators=[MaxValueValidator(99), MinValueValidator(1)])
+    direccion = models.CharField(max_length= 200, null = False)
+    curp = models.CharField(max_length= 16,validators=[MinLengthValidator(16)])
 
     def __str__(self):
         return self.name,self.edad,self.direccion,self.curp
